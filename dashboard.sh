580e1ed85135087bb81a1b86c01f82778489aefa
lfrati <lfrati@uvm.edu>
2020-04-09 17:55:07 -0400

    Tweaks for new home setup

--- dashboard.sh
+++ dashboard.sh
@@ -3,7 +3,7 @@
 #
 # Setup a work space called `dash` with 4 panes
 # The first pane set at 65%, split horizontally, set to api root and running vim
-# pane 2 is split at 25% and running redis-server 
+# pane 2 is split at 25% and running redis-server
 # pane 3 is set to api root and bash prompt.
 # note: `api` aliased to `cd ~/path/to/work`
 #
@@ -18,35 +18,45 @@ if [ $? != 0 ]; then
   # set up tmux
   tmux start-server
 
+  # Pane 1
   # create a new tmux session
   tmux new-session -d -s $session
 
+  # Pane 2
   # Split pane 1 horizontally to make room for GPU/CPU info
   tmux splitw -h -p 10
-  # Split vertically for small CPU info
-  tmux splitw -v -p 55
 
+  # Pane 3
+  # Taskwarrior pane
+  tmux splitw -v -p 50
+
+  # Pane 4
+  # Split for small CPU info
+  tmux splitw -v -p 25
+
+  # Pane 5
   # Make thin bottom pane for music
   tmux splitw -f -v -p 5
 
-  # Taskwarrior pane
+  # Pane 6
   tmux splitw -h
 
-  # Assign tasks to each pane 
-  tmux selectp -t 1 
-  tmux send-keys "htop" C-m 
+  # Assign tasks to each pane
+  tmux selectp -t 1
+  tmux send-keys "htop" C-m
 
   tmux selectp -t 2
-  tmux send-keys "watch --no-title -n 5 nvidia-smi" C-m 
+  tmux send-keys "watch --no-title -n 5 nvidia-smi" C-m
 
   tmux selectp -t 3
-  tmux send-keys "watch --no-title -n 5 sensors" C-m 
+  tmux send-keys "while true; do clear && task next limit:3; sleep 3; done" C-m
 
   tmux selectp -t 4
-  tmux send-keys "ncmpcpp" C-m 
+  tmux send-keys "watch --no-title -n 5 sensors -A k10temp-pci-00c3" C-m
 
   tmux selectp -t 5
-  tmux send-keys "while true; do clear && task next limit:3; sleep 3; done" C-m 
+  tmux send-keys "ncmpcpp" C-m
+
 
 fi
 
